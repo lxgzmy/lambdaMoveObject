1c814aa0ab147c32fe4ae2629e0dbc7f9ea93139
Jason Liu <jason.liu@cashrewards.com>
2018-07-16 10:19:04 +1000

    fix deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 rm output.zip
 zip -r -q output.zip ./*
-aws lambda update-function-code --function-name CrossAccountCopyForBuildArtifact --zip-file fileb://output.zip --profile crdev
+aws lambda update-function-code --function-name CrossAccountCopyForBuildArtifact --zip-file fileb://output.zip environment Variables="{Dest_S3_Bucket=cashrewards.serverless.build.artifact.production}" --profile crdev
